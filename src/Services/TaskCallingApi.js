50a2596af70550c99e4bcb7e173988fc3b37b5a0
Duc Vinh Le <vinhxp95@gmail.com>
2022-04-17 12:59:58 +1000

    jira drag drop finish

--- src/Services/TaskCallingApi.js
+++ src/Services/TaskCallingApi.js
@@ -1,74 +1,75 @@
 import Axios from "axios";
 import { TOKEN } from "../utilities/Constants";
 export class TaskCallingApi {
-    constructor(){
+    constructor() {
 
     }
-    getTaskTypeService(){
+    getTaskTypeService() {
         return Axios({
-            url:`http://casestudy.cyberlearn.vn/api/TaskType/getAll`,
+            url: `http://casestudy.cyberlearn.vn/api/TaskType/getAll`,
             method: "GET",
         })
     }
-    getTaskPriorityService(){
+    getTaskPriorityService() {
         return Axios({
-            url:`http://casestudy.cyberlearn.vn/api/Priority/getAll`,
-            method:`GET`
+            url: `http://casestudy.cyberlearn.vn/api/Priority/getAll`,
+            method: `GET`
         })
     }
-    getTaskStatusService(){
+    getTaskStatusService() {
         return Axios({
-            url:`http://casestudy.cyberlearn.vn/api/Status/getAll`,
-            method:`GET`
+            url: `http://casestudy.cyberlearn.vn/api/Status/getAll`,
+            method: `GET`
         })
     }
-    createTaskService(newTask){
-        console.log("Service",newTask)
+    createTaskService(newTask) {
+        console.log("Service", newTask)
         return Axios({
-            url:`http://casestudy.cyberlearn.vn/api/Project/createTask`,
-            method:`POST`,
+            url: `http://casestudy.cyberlearn.vn/api/Project/createTask`,
+            method: `POST`,
             data: newTask,
-            headers: {'Authorization': "Bearer " + localStorage.getItem(TOKEN)}
+            headers: { 'Authorization': "Bearer " + localStorage.getItem(TOKEN) }
         })
     }
     //======= GET TASK DETAIL ====
-    getTaskDetailByIdService(id){
+    getTaskDetailByIdService(id) {
         console.log("Getting task detail of " + id);
         return Axios({
-            url:`http://casestudy.cyberlearn.vn/api/Project/getTaskDetail?taskId=${id}`,
-            method:`GET`,
-            headers:{'Authorization': "Bearer " + localStorage.getItem(TOKEN)}
+            url: `http://casestudy.cyberlearn.vn/api/Project/getTaskDetail?taskId=${id}`,
+            method: `GET`,
+            headers: { 'Authorization': "Bearer " + localStorage.getItem(TOKEN) }
         })
     }
     // ======= UPDATE TASK STATUS ======
-    updateTaskStatus(newTask){
+    updateTaskStatus(newTask) {
         console.log("updating task", newTask);
         return Axios({
-            url:`http://casestudy.cyberlearn.vn/api/Project/updateStatus`,
-            method:`PUT`,
+            url: `http://casestudy.cyberlearn.vn/api/Project/updateStatus`,
+            method: `PUT`,
             data: newTask,
-            headers:{'Authorization': "Bearer " + localStorage.getItem(TOKEN)}
+            headers: { 'Authorization': "Bearer " + localStorage.getItem(TOKEN) }
         })
     }
 
     // ======= UPDATE TASK DESCRIPTION =======
-    updateTaskDescriptionService(newDescription){
+    updateTaskDescriptionService(newDescription) {
         return Axios({
-            url:`http://casestudy.cyberlearn.vn/api/Project/updateDescription`,
-            method:`PUT`,
+            url: `http://casestudy.cyberlearn.vn/api/Project/updateDescription`,
+            method: `PUT`,
             data: newDescription,
-            headers:{'Authorization': "Bearer " + localStorage.getItem(TOKEN)}
+            headers: { 'Authorization': "Bearer " + localStorage.getItem(TOKEN) }
         })
     }
 
-    updateEntireTaskService(updatedTask){
+    updateEntireTaskService(updatedTask) {
         return Axios({
-            url:`http://casestudy.cyberlearn.vn/api/Project/updateTask`,
-            method:`POST`,
+            url: `http://casestudy.cyberlearn.vn/api/Project/updateTask`,
+            method: `POST`,
             data: updatedTask,
-            headers:{'Authorization': "Bearer " + localStorage.getItem(TOKEN)}
+            headers: { 'Authorization': "Bearer " + localStorage.getItem(TOKEN) }
         })
     }
+
 
 }
 
--- src/components/Main/ContentMain.js
+++ src/components/Main/ContentMain.js
@@ -1,63 +1,114 @@
 import Avatar from 'antd/lib/avatar/avatar';
 import React from 'react'
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 import InfoModal from '../../components/Main/InfoModal'
-import { GET_TASK_DETAIL_BY_ID } from '../../Redux/ReduxTypeList/typeList';
+import { GET_TASK_DETAIL_BY_ID, UPDATE_STATUS_TO_API } from '../../Redux/ReduxTypeList/typeList';
 import { getTaskDetailById } from '../../Redux/Saga/SagaActionList/ActionTaskSagaList';
 
 export default function ContentMain(props) {
-   
+
     const projectDetail = useSelector(state => state.ProjectStateReducer.projectInfo);
+    //console.log("projectDetail",projectDetail)
     const dispacth = useDispatch();
+    const handleDragEnd = (res) => {
+        console.log(res);
+        if (!res.destination){
+            return ;
+        }
+        if (res.source.index === res.destination.index && res.source.droppableId === res.destination.droppableId){
+            return ;
+        }
+        dispacth({
+            type:UPDATE_STATUS_TO_API,
+            newStatus: {
+                taskId: res.draggableId,
+                statusId: res.destination.droppableId,
+                projectId: projectDetail.id
+            }
+        })
+
+    }
     // ==== renderBackLog =====
     const renderCardTaskList = () => {
-        return projectDetail.lstTask?.map((taskList, index) => {
-            return <div key={index} className="card" style={{ width: '17rem', height: '25rem' }}>
-                <div className="card-header">
-                    {taskList.statusName}
-                </div>
-                <ul className="list-group list-group-flush">
-                    {taskList.lstTaskDeTail?.map((item, index) => {
-                        return <li key={index} className="list-group-item" data-toggle="modal" data-target="#infoModal" style={{ cursor: 'pointer' }} onClick={()=>{
-                            dispacth({
-                                type: GET_TASK_DETAIL_BY_ID,
-                                id: item.taskId
-                            })
-
-                        }}>
-                            <p className='font-weight-bold'>
-                                {item.taskName}
-                            </p>
-                            <div className="block" style={{ display: 'flex' }}>
-                                <div className="block-left">
-                                    {item.priorityTask.priority}
-                                </div>
-                                <div className="block-right">
-                                    <div className="avatar-group" style={{ display: 'flex' }}>
-                                        {
-                                            item.assigness.slice(0, 3).map((member, index) => {
-                                                return <div className="avatar" key={index}>
-                                                    <img src={member.avatar} alt="..." />
-                                                </div>
-                                            })
-                                          
-                                        }
-                                        {item.assigness.length > 3 ? <Avatar>...</Avatar> : ''}
-                                </div>
+        return <DragDropContext onDragEnd={(res) => {
+            handleDragEnd(res)
+        }}>
+            {projectDetail.lstTask?.map((taskList, index) => {
+                // console.log(taskList)
+                return <Droppable droppableId={taskList.statusId} key={index}>
+                    {(provided) => {
+                        return <div key={index}
+                            className="card"
+                            style={{ width: '17rem', height: '25rem' }}
+                        >
+                            <div className="card-header">
+                                {taskList.statusName}
                             </div>
-                        </div>
-                       
-                        </li>
-                    })}
-            </ul>
-            </div >
-        })
+                            <div ref={provided.innerRef}
+                                {...provided.droppableProps} style={{width:"100%"}}>
+                                <ul className="list-group list-group-flush"
+                                >
+                                    {taskList.lstTaskDeTail?.map((item, index) => {
+                                        //console.log(item);
+                                        return <Draggable key={item.taskId} draggableId={item.taskId.toString()} index={index}>
+                                            {(provided) => {
+                                                return <li key={index} className="list-group-item" data-toggle="modal" data-target="#infoModal" style={{ cursor: 'pointer' }} onClick={() => {
+                                                    dispacth({
+                                                        type: GET_TASK_DETAIL_BY_ID,
+                                                        id: item.taskId
+                                                    })
+                                                }}
+                                                    ref={provided.innerRef}
+                                                    {...provided.draggableProps}
+                                                    {...provided.dragHandleProps}>
+                                                    <p className='font-weight-bold'>
+                                                        {item.taskName}
+                                                    </p>
+                                                    <div className="block" style={{ display: 'flex' }}>
+                                                        <div className="block-left">
+                                                            {item.priorityTask.priority}
+                                                        </div>
+                                                        <div className="block-right">
+                                                            <div className="avatar-group" style={{ display: 'flex' }}>
+                                                                {
+                                                                    item.assigness.slice(0, 3).map((member, index) => {
+                                                                        return <div className="avatar" key={index}>
+                                                                            <img src={member.avatar} alt="..." />
+                                                                        </div>
+                                                                    })
+
+                                                                }
+                                                                {item.assigness.length > 3 ? <Avatar>...</Avatar> : ''}
+                                                            </div>
+                                                        </div>
+                                                    </div>
+
+                                                </li>
+                                            }
+                                            }
+
+                                        </Draggable>
+
+                                    })}
+                                </ul>
+                            </div>
+
+                            {provided.placeholder}
+                        </div >
+                    }
+                    }
+                </Droppable>
+
+            })}
+        </DragDropContext>
+
     }
-return (
-    <div className="content" style={{ display: 'flex' }}>
-        {renderCardTaskList()}
-        {/* <div className="card" style={{ width: '17rem', height: '25rem' }}>
+    return (
+        <div className="content" style={{ display: 'flex' }}>
+            {renderCardTaskList()}
+            {/* <div className="card" style={{ width: '17rem', height: '25rem' }}>
                 <div className="card-header">
                     SELECTED FOR DEVELOPMENT 2
                 </div>
@@ -85,8 +136,8 @@ return (
                     <li className="list-group-item">Vestibulum at eros</li>
                 </ul>
     </div>*/}
-   
-    </div>
 
-)
+        </div>
+
+    )
 }
